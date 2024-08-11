b6f9e890bc34b4d75b039ffbb031da24cb5647a2
vanech20 <estefyvane2007@gmail.com>
2024-08-10 19:59:24 -0600

    Initial Commit

---
+++ mathmodeling/mathmodelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathmodelingApp.swift
+//  mathmodeling
+//
+//  Created by Desarrollo on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct mathmodelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
